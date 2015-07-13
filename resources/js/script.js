6b2d0ea10ae9cf6ffb8a000c59fa0b3aa99cdfcf
Wajid Kagzi <wjkagzi@gmail.com>
2015-07-13 17:23:46 -0400

    Added animations to scrolling.

--- resources/js/script.js
+++ resources/js/script.js
@@ -33,4 +33,30 @@ $(document).ready(function() {
       }
     }
   });
+
+  // Animations on scroll
+  $('.js--wp-1').waypoint(function() {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function() {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function() {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function() {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
+  });
+
 });
