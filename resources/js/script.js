38c2312920600ad9f07b8992ac21950dd4fd315c
Wajid Kagzi <wjkagzi@gmail.com>
2015-07-13 17:03:03 -0400

    Added smooth navigation scroll.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,36 @@
 $(document).ready(function() {
+
+  // For the sticky navigation
   $('.js--section-features').waypoint(function(direction) {
     if (direction == 'down') {
       $('nav').addClass('sticky');
     } else {
       $('nav').removeClass('sticky');
     }
-  },
+  }, {
+    offset: 60
+  });
 
-  { offset: '60px'
+  // Scroll on buttons
+  $('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-features').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+
+  // Smooth navigation scroll
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
